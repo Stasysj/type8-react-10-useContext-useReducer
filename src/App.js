00e90a2b86b747630dd613b41b329fb6fe4bf04a
Marius <marius@example.com>
2022-06-17 09:35:34 +0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Counter from './components/Counter/Counter';
 import Login from './components/Login';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className='App'>
+      <Navigation />
       <Login />
       <Counter />
     </div>
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import css from './Navigation.module.css';
+
+function Navigation() {
+  return (
+    <nav className={css.nav}>
+      <a href='#'>Home</a>
+      <a href='#'>Login</a>
+      <a href='#'>Logout</a>
+    </nav>
+  );
+}
+export default Navigation;
