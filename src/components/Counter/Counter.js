34b6ed266d3ac43acfd3227c438dc58d049a2ff8
Marius <marius@example.com>
2022-06-17 11:48:53 +0300

    inc dec

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,20 +1,37 @@
 import css from './Counter.module.css';
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import Card from '../Card/Card';
 
+const initCounterValue = { count: 0 };
+
+function counterReducer(state, action) {
+  console.log('state ===', state);
+  console.log('action ===', action);
+
+  if (action === '-') {
+    return { count: state.count - 1 };
+  }
+
+  return { count: state.count + 1 };
+}
+// state.count++ === state.count = state.count + 1
 function Counter() {
-  const [value, setValue] = useState(0);
+  // const [state, setValue] = useState({ count: 0 });
+
+  const [state, dispatch] = useReducer(counterReducer, initCounterValue);
 
   function handleInc() {
-    setValue((prevValue) => prevValue + 1);
+    // setValue((prevValue) => prevValue + 1);
+    dispatch('+');
   }
   function handleDec() {
-    setValue((prevValue) => prevValue - 1);
+    // setValue((prevValue) => prevValue - 1);
+    dispatch('-');
   }
 
   return (
     <div>
-      <h2 className={css.count}>{value}</h2>
+      <h2 className={css.count}>{state.count}</h2>
       <button onClick={handleInc}>Increase</button>
       <button onClick={handleDec}>Down</button>
       <Card />
