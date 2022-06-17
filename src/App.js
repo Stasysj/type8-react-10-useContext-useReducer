50ecc5ad453ab2e36285d18884a86ede88535519
Marius <marius@example.com>
2022-06-17 10:01:13 +0300

    isLoggedInUzd

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import Counter from './components/Counter/Counter';
 import Login from './components/Login';
 import Navigation from './components/Navigation/Navigation';
 import AuthContext from './store/authContext';
+import { useState } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // paduoti isLoggedIn i contexta
+  // pasiimti ctx Navigation
+  // priklausomai ar trua ar false rodom login arba logout
+  const ctx = {
+    userName: 'James Smith',
+    age: 36,
+  };
   return (
-    <AuthContext.Provider value={'James bond'}>
+    <AuthContext.Provider value={ctx}>
       <div className='App'>
         <Navigation />
         <Login />
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
 import css from './Card.module.css';
+import AuthContext from './../../store/authContext';
 
 function Card(props) {
+  const ctx = useContext(AuthContext);
+  console.log('ctx ===', ctx);
+
   return (
     <div className={css.card}>
       {props.children}
-      <h3>I am card</h3>
+      <h3>The name in context value is: {ctx.userName}</h3>
       <button>Logout</button>
     </div>
   );
