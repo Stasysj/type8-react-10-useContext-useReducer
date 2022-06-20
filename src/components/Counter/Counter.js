509ea9d7ef58bcd28fc351eb4b69c95064808961
Stasys <srasys@st.lt>
2022-06-20 09:09:50 +0300

    daugiau uz 5

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,6 +2,13 @@ import css from './Counter.module.css';
 import { useReducer, useState } from 'react';
 import Card from '../Card/Card';
 
+const ACTIONs = {
+  INCR: 'INCR',
+  DECR: 'DECR',
+  DOWN5: 'DOWN5',
+  UPBY: 'UPBY',
+};
+
 const initCounterValue = { count: 0 };
 
 function counterReducer(state, action) {
@@ -9,11 +16,21 @@ function counterReducer(state, action) {
   console.log('action ===', action);
 
   switch (action.type) {
-    case 'INCR':
+    case ACTIONs.INCR:
+      if (state.count === 4) {
+        const stateCopy = { ...state };
+        stateCopy.error = 'Negalima reiksme didesne uz 5';
+        return stateCopy;
+      }
       return { count: state.count + 1 };
     case 'UPBY':
       return { count: state.count + action.payload };
     case 'DECR':
+      if (state.count === 0) {
+        const stateCopy = { ...state };
+        stateCopy.error = 'Negalima neigiama reikshme';
+        return stateCopy;
+      }
       return { count: state.count - 1 };
     case 'DOWN5':
       return { count: state.count - 5 };
@@ -31,7 +48,7 @@ function Counter() {
 
   function handleInc() {
     // setValue((prevValue) => prevValue + 1);
-    dispatch({ type: 'INCR' });
+    dispatch({ type: ACTIONs.INCR });
   }
   function handleDec() {
     // setValue((prevValue) => prevValue - 1);
@@ -43,11 +60,14 @@ function Counter() {
   return (
     <div>
       <h2 className={css.count}>{state.count}</h2>
-      <button onClick={() => dispatch({ type: 'UPBY', payload: 7 })}>Up by 7</button>
-      <button onClick={handleInc}>Increase</button>
+      {state.error && <h3>{state.error}</h3>}
+
+      {/* <button onClick={() => dispatch({ type: 'UPBY', payload: 7 })}>Up by 7</button> */}
+      {state.count < 5 && <button onClick={handleInc}>Increase</button>}
+      {/* <button onClick={handleInc}>Increase</button> */}
       <button onClick={handleDec}>Down</button>
-      <button onClick={() => dispatch({ type: 'DOWN5' })}>down 5</button>
-      <button onClick={() => dispatch({ type: 'reset' })}>RESET</button>
+      {/* <button onClick={() => dispatch({ type: 'DOWN5' })}>down 5</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>RESET</button> */}
       <Card />
     </div>
   );
