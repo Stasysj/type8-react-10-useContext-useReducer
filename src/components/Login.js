e18681ca145ab7e1ba06443939d7616625cc68bd
Stasys <srasys@st.lt>
2022-06-20 14:53:23 +0300

    Pirmine valid dstart

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,29 @@
 // import css from 'Login.module.css';
 import AuthContext from './../store/authContext';
 import { useContext, useRef } from 'react';
+import { useReducer } from 'react';
+
+const initValues = {
+  username: '',
+  password: '',
+  usernameErr: '',
+  passwordErr: '',
+};
+function loginReducerFn(state, action) {
+  switch (action.type) {
+    case 'password':
+      return { ...state, password: action.payload };
+    case 'username':
+      return { ...state, username: action.payload };
+
+    default:
+      throw new Error('klaida');
+  }
+}
 
 function Login() {
+  // TODO: Login abu inputus valdom su vienu useReducer state
+  const [state, dispatch] = useReducer(loginReducerFn, initValues);
   const ctx = useContext(AuthContext);
 
   const userNameRef = useRef();
@@ -21,8 +42,19 @@ function Login() {
     <div>
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
-        <input ref={userNameRef} type='text' placeholder='username' />
-        <input type='password' placeholder='password' />
+        <input
+          onChange={(e) => dispatch({ type: 'username', payload: e.target.value })}
+          ref={userNameRef}
+          type='text'
+          placeholder='username'
+          value={state.username}
+        />
+        <input
+          onChange={(e) => dispatch({ type: 'password', payload: e.target.value })}
+          type='password'
+          placeholder='password'
+          value={state.password}
+        />
         <button type='submit'>Send</button>
       </form>
     </div>
